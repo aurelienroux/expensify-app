335d977d10c62d2afa522227899f9374598100d3
Aurélien Roux <aroux@clever-age.com>
2019-06-29 20:44:28 -0400

    setting up react router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 
-ReactDOM.render(<p>boilerplate</p>, document.getElementById('app'));
+const ExpensesDashboardPage = () => (
+  <div>
+    dashboard
+  </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    add expenses
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    edit expenses
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    Help me
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpensesDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'));
