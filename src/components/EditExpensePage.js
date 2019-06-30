7df83568f9578b7056bf567316ac94db2f0ec6b8
Aurélien Roux <aroux@clever-age.com>
2019-06-30 13:08:03 -0400

    passing params in url router

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const EditExpensePage = () => (
+const EditExpensePage = (props) => (
   <div>
-    edit expenses!
+    edit expenses number {props.match.params.id}
   </div>
 );
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => (
     <h1>Expensify!</h1>
     <NavLink to="/" exact={true} activeClassName="is-active">Homepage </NavLink>
     <NavLink to="/create" activeClassName="is-active">Create </NavLink>
-    <NavLink to="/edit" activeClassName="is-active">Edit </NavLink>
     <NavLink to="/help" activeClassName="is-active">Help </NavLink>
   </header>
 );
