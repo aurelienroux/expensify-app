473971ef5023f05bc6d34fb502a941f83c126321
Aurélien Roux <aureroux13@gmail.com>
2019-07-15 18:12:39 +0200

    actions organisation and refacto

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,16 +1,36 @@
 import { createStore } from 'redux';
 
+
+//Actions generators
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: 'INCREMENT',
+  incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+});
+
+const setCount = ({ newCount } = {}) => ({
+  type: 'SET',
+  newCount
+});
+
+const resetCount = () => ({
+  type: 'RESET'
+});
+
+// Store setup
 const store = createStore((state = { count : 0 }, action ) => {
   switch (action.type) {
     case 'INCREMENT':
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       }
     case 'DECREMENT':
-      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - decrementBy
+        count: state.count - action.decrementBy
       }
     case 'RESET':
       return {
@@ -29,30 +49,29 @@ const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
-// increment the count
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 6
-});
+// store.dispatch({
+//   type: 'INCREMENT',
+//   incrementBy: 6
+// });
+store.dispatch(incrementCount({ incrementBy: 4 }))
 
-store.dispatch({
-  type: 'INCREMENT',
-});
+store.dispatch(incrementCount());
 
-store.dispatch({
-  type: 'RESET'
-})
+// store.dispatch({
+//   type: 'RESET'
+// })
+store.dispatch(resetCount());
 
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 3
-})
+// store.dispatch({
+//   type: 'DECREMENT',
+//   decrementBy: 3
+// })
+store.dispatch(decrementCount({ decrementBy: 14 }));
 
-store.dispatch({
-  type: 'SET',
-  newCount: 101
-})
+// store.dispatch({
+//   type: 'SET',
+//   newCount: 101
+// })
+store.dispatch(setCount({ newCount: 76 }))
 
-store.dispatch({
-  type: 'DECREMENT'
-})
+store.dispatch(decrementCount());
