c492d4840042c2ba64fed10ae57252ffb7816775
Aurélien Roux <aureroux13@gmail.com>
2019-07-18 19:31:08 +0200

    connect component expenselist to redux

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 
+import { Provider } from 'react-redux';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 
@@ -24,4 +26,10 @@ const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'));
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <h1>Expensify!</h1>
+    <h1>Expensify</h1>
     <NavLink to="/" exact={true} activeClassName="is-active">Homepage </NavLink>
     <NavLink to="/create" activeClassName="is-active">Create </NavLink>
     <NavLink to="/help" activeClassName="is-active">Help </NavLink>
