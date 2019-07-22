b84e6cb8e1a0a85523071343c8aa9155f30df565
Aurélien Roux <aureroux13@gmail.com>
2019-07-22 15:35:00 +0200

    sort by amount with controlled input store connect

--- src/app.js
+++ src/app.js
@@ -15,20 +15,14 @@ import './styles/styles.scss'
 const store = configureStore();
 
 // Add bills
-store.dispatch(addExpense({description: 'water bill', amount: 1234, createdAt: 1200}));
-store.dispatch(addExpense({description: 'gaz bill'}));
-
-// set text filter with bill
-store.dispatch(setTextFilter('water'));
-
-setTimeout(() => {
-  store.dispatch(setTextFilter('bill'));
-}, 3000);
+store.dispatch(addExpense({ description: 'water bill', amount: 4500 }));
+store.dispatch(addExpense({ description: 'gaz bill', createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'rent', amount: 109500 }));
 
 // get visible expenses 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
