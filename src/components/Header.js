baa1b4354f8af0f67d11ae8bfdabb136bb517aba
Aurélien Roux <aureroux13@gmail.com>
2019-07-31 14:23:38 -0400

    trigger auto deploy heroku

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <h1>Expensify</h1>
+    <h1>Expensify App</h1>
     <NavLink to="/" exact={true} activeClassName="is-active">Homepage </NavLink>
     <NavLink to="/create" activeClassName="is-active">Create </NavLink>
   </header>
