f82f9fb504a5df4a445d15a146467ab594300db2
Aurélien Roux <aureroux13@gmail.com>
2019-08-01 14:25:17 -0400

    firebase config and connection

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ import 'react-dates/initialize';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+import './firebase/firebase';
+
 const store = configureStore();
 
 const jsx = (
---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDz32J-Qj6-LgLXan5JgYNg9D20UZzZt0",
+  authDomain: "expensify-9deae.firebaseapp.com",
+  databaseURL: "https://expensify-9deae.firebaseio.com",
+  projectId: "expensify-9deae",
+  storageBucket: "expensify-9deae.appspot.com",
+  messagingSenderId: "867802824954",
+  appId: "1:867802824954:web:caf5e2ce5bca605b"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+firebase.database().ref().set({
+  name: 'testing app'
+});
