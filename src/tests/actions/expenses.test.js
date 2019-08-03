d3f07487268f22cdbc6d0e3c61474722c57ce02a
Aurélien Roux <aureroux13@gmail.com>
2019-08-03 15:03:08 -0400

    testing async redux actions

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,4 +1,9 @@
-import { removeExpense, editExpense, addExpense } from '../../actions/expenses';
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk';
+import { startAddExpense, removeExpense, editExpense, addExpense } from '../../actions/expenses';
+import expenses from '../fixtures/expenses';
+
+const createMockStore = configureMockStore([thunk]);
 
 test('should setup remove action object', () => {
   const action = removeExpense({ id:'123abc' });
@@ -20,32 +25,42 @@ test('should setup edit action object', () => {
 });
 
 test('should setup add expense action object with provided values', () => {
-  const expenseData = {
-    amount: 109500,
-    createdAt: 1000,
-    description: 'rent',
-    note: 'this is a note'
-  }
-  const action = addExpense(expenseData);
+  const action = addExpense(expenses[2]);
   expect(action).toEqual({
     type: 'ADD_EXPENSE',
-    expense: {
-      ...expenseData,
-      id: expect.any(String)
-    }
+    expense: expenses[2]
   })
 });
 
-test('should setup add expense action object with default values', () => {
-  const action = addExpense();
-  expect(action).toEqual({
-    type: 'ADD_EXPENSE',
-    expense: {
-      amount: 0,
-      createdAt: 0,
-      description: '', 
-      id: expect.any(String),
-      note: ''
-    }
-  })
+test('should add expense to database and store', (done) => {
+  const store = createMockStore({});
+  const expenseData = {
+    description: 'mouse',
+    amount: 10,
+    note: ' a mouse note',
+    createdAt: 1000
+  };
+
+  store.dispatch(startAddExpense(expenseData)).then(() => {
+    expect(1).toBe(2);
+    done();
+  });
 });
+
+test('should add expense with defaults to database and store', () => {
+  
+});
+
+// test('should setup add expense action object with default values', () => {
+//   const action = addExpense();
+//   expect(action).toEqual({
+//     type: 'ADD_EXPENSE',
+//     expense: {
+//       amount: 0,
+//       createdAt: 0,
+//       description: '', 
+//       id: expect.any(String),
+//       note: ''
+//     }
+//   })
+// });
