6ae907a11bbdf0e63c3a5840a90bf94db4f3ddef
Aurélien Roux <aureroux13@gmail.com>
2019-08-07 09:39:00 -0400

    login with Google auth

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import 'react-dates/lib/css/_datepicker.css';
 import 'react-dates/initialize';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 
 const store = configureStore();
 
@@ -24,4 +24,12 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 store.dispatch(startSetExpenses()).then(() => {
   ReactDOM.render(jsx, document.getElementById('app'));
+});
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log('in');
+  } else {
+    console.log('out');
+  }
 });
