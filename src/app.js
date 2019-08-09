b1f86068032239efc7622e992659aaeb70fe4d9c
Aurélien Roux <aureroux13@gmail.com>
2019-08-09 12:12:50 -0400

    auth reducer

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter, { history } from './routes/AppRouter';
-import { startSetExpenses } from './actions/expenses'
-
-import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
-
+import AppRouter, { history } from './routes/AppRouter';
+import { startSetExpenses } from './actions/expenses';
+import { login, logout } from './actions/auth';
+import configureStore from './store/configureStore';
 import 'react-dates/lib/css/_datepicker.css';
 import 'react-dates/initialize';
 import 'normalize.css/normalize.css';
@@ -31,6 +30,7 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
+    store.dispatch(login(user.uid));
     store.dispatch(startSetExpenses()).then(() => {
       renderApp();
       if (history.location.pathname === '/') {
@@ -38,6 +38,7 @@ firebase.auth().onAuthStateChanged((user) => {
       }
     });
   } else {
+    store.dispatch(logout());
     renderApp();
     history.push('/');
   }
