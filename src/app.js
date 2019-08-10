e9b123bd105ea10c9bea74ba1a178016df09bf49
Aurélien Roux <aureroux13@gmail.com>
2019-08-10 15:57:03 -0400

    update snapshots and create loader comp

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import 'react-dates/initialize';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage.js';
 
 const store = configureStore();
 
@@ -26,7 +27,7 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
